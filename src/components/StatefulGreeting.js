3ee40e857bd5e45272a2d00b03b2899a381044b5
Yuss76A <yuse_91yus@hotmail.com>
2025-03-05 14:21:46 +0100

    Finish the updating the State, part 4

--- src/components/StatefulGreeting.js
+++ src/components/StatefulGreeting.js
@@ -7,20 +7,39 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "Hello!",
             buttonText: "Exit",
+            count: 0
         };
     }
 
     handleClick() {
-        this.setState({
-            introduction: "Goodbye",
-            buttonText: "Enter"
-        }, ()=> {
-        console.log('new.state', this.state.introduction);
-        console.log('new.state', this.state.buttonText);
+        this.setState((prevState, prevProps) => {
+          console.log("Previous State:", prevState);
+          console.log("Previous Props:", prevProps);
+          return {
+            introduction:
+              prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+          };
         });
-        console.log(this.state.introduction);
-        console.log(this.state.buttonText);
+      }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:' , prevState)
+            console.log('Previous Props:' , prevProps)
+            return {
+            count: this.state.count + 1
+            };
+        });
     }
+
+    incrementFive() {
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+      }
 
 
     render() {
@@ -28,6 +47,8 @@ class StatefulGreeting extends React.Component {
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
 
         )
