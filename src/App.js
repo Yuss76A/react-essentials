eaf6f1d8f170f42f8db80e21ebb538281b8087bb
Yuss76A <yuse_91yus@hotmail.com>
2025-03-05 14:00:38 +0100

    Learn updating the state, part2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import StatefulGreeting from "./components/StatefulGreeting";
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting />
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Mike"/>
     </div>
   );
 }
--- src/components/StatefulGreeting.js
+++ src/components/StatefulGreeting.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye",
+            buttonText: "Enter"
+        }, ()=> {
+        console.log('new.state', this.state.introduction);
+        console.log('new.state', this.state.buttonText);
+        });
+        console.log(this.state.introduction);
+        console.log(this.state.buttonText);
+    }
+
+
     render() {
-        return <h1>Hello from the stateful componnent</h1>
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+
+        )
     }
 }
 
